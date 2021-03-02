dd1e6950835401330852aaa14013f6660d91cb6d
Anoux Jadur <ajadur96@gmail.com>
2021-03-02 05:49:08 -0300

    django update|tuesday 6 am

--- products/forms.py
+++ products/forms.py
@@ -12,3 +12,19 @@ class ProductForm(forms.ModelForm):
 			'description',
 			'price'
 		]
+
+class RawProductForm(forms.Form):
+	title = forms.CharField(label='', widget=forms.TextInput(attrs={"placeholder": "Type your title here"}))
+	description = forms.CharField(
+						required=False, 
+						widget=forms.Textarea(
+								attrs={
+									"placeholder": "Type your title here",
+									"class": "new-class-name two",
+									"id": "my-id-for-textarea",
+									"rows": 10,
+									"cols": 50
+								}
+							)
+						)
+	price = forms.CharField(initial=199.99)
